995232478536fc4ed62b9d69aacea42d8f09ecfb
JaYdEnTaNg123 <tangjayden999@gmail.com>
2024-02-24 11:49:17 +0800

    this is not me

--- main.py
+++ main.py
@@ -4,20 +4,42 @@ Q1:
 '''
 
 # TODO: Write your code here
+def cube_a_number(x):
+    result = x * x * x
+    return result
 
+cubed_res = cube_a_number(10)
+print(f'The result is {cubed_res}.')
 ##################################################
 '''
 Q2:
 '''
 
 # TODO: Write your code here
-
+def is_even(n):
+    if n % 2 == 0:
+       return True 
+    else:
+        return False
+result = is_even(13)    # result => True or False
+print(f'13 is even: {result}')
 ##################################################
 '''
 Q3:
 '''
 
 # TODO: Write your code here
+def is_even(n):
+    return(n % 2 == 0)
+result = is_even(13)    # result => True or False
+print(f'13 is even: {result}')
+##################################################
+'''
+Q4:
+'''
+
+# TODO: Write your code here
+
 
 ##################################################
 '''
